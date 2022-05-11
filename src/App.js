507d30441e73644b49029d0d9c281827e444bded
Tringapps-sherin <102800904+Tringapps-sherin@users.noreply.github.com>
2022-05-11 11:42:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { TextField } from '@mui/material';
+import { useState } from 'react';
+import { Button } from '@mui/material';
+import {Box, Container } from '@mui/material';
+import FormDisplay from './components/FormDisplay';
+function App() {
+  const [displayDetails,setDisplayDetails] = useState([]);
+  
+  const [pname,setProductName] = useState('');
+  const [pprice,setProductPrice] = useState('');
+    function handleSubmit(event){
+    event.preventDefault();
+    setDisplayDetails([...displayDetails,{ProductName :pname,ProductPrice :pprice}])
+    setProductName('')
+    setProductPrice('')
+
+  }
+  console.log(displayDetails);
+  return (
+    <div>
+      <div><Box sx ={{textAlign:'center',fontSize:'40px',padding:'20px',}}>AYYANAR STORE</Box>
+    <Container sx = {{backgroundColor:'#87CEFA',padding:'30px',height:'50',width:'100',border:'1px solid black'}}>
+      <form onSubmit = {handleSubmit}>
+        <div><TextField color='secondary' fullWidth  label = 'Product Name' varient = 'outlined' value = {pname} onChange={(event)=>setProductName(event.target.value)} required/></div>
+        <div><TextField color='secondary'  fullWidth  label = 'Product Price' value = {pprice} varient = 'outlined' onChange={(event)=>setProductPrice(event.target.value)} required/></div>
+        <div><Button type = 'submit' variant="contained" sx ={{textAlign:'center'}}>UPLOAD</Button> </div>
+        </form>
+        </Container>
+        </div>
+      <div>
+      <FormDisplay displayDetails = {displayDetails} setDisplayDetails = {setDisplayDetails} setProductName = {setProductName} setProductPrice = {setProductPrice} />
+      </div>
+
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/FormDisplay.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Card, CardContent, Typography, CardActions, Button, CardMedia, CardHeader, IconButton, Menu, MenuItem, Grid,Dialog,DialogActions,DialogTitle,DialogContent,DialogContentText} from '@mui/material';
+import Items from '../assets/Items.jpg'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from "react";
+import '../App.css'
+export default function FormDisplay({ displayDetails, setDisplayDetails, setProductName, setProductPrice }) {
+
+    const [dialogOpen, setOpen] = useState(false);
+    const [storeIndex,setStoreIndex] = useState(null);
+    const [editProduct,setEditProduct] = useState(null);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClickClose = () => {
+        setOpen(false);
+    };
+
+    const handleOk = () => {
+        handleDelete();
+        setOpen(false);
+    }
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClickIcon = (event,index,product) => {
+        setAnchorEl(event.currentTarget);
+        setStoreIndex(index);
+        setEditProduct(product)
+    }
+    const handleClose = () => {
+
+        setAnchorEl(null);
+    }
+    const handleEdit = () => {
+        setProductName(editProduct.ProductName)
+        setProductPrice(editProduct.ProductPrice)
+        handleDelete();
+    }
+
+    const handleDelete = () => {
+        displayDetails.splice(storeIndex,1);
+        setDisplayDetails(displayDetails)
+        handleClose()
+    }
+
+    return (
+        <>
+            <Grid container spacing={3}>
+                {
+                    displayDetails.map((product,index) => {
+                        return (
+                            <Grid item xs={3}>
+                            <div key={index}>
+                                <Menu id='menus' anchorEl={anchorEl} open={open} MenuListProps={{
+                                    'aria-labelledby': 'iconId',
+                                }}
+                                    onClose={handleClose}>
+                                    <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                                    <MenuItem onClick={handleClickOpen}>Delete</MenuItem>
+                                </Menu>
+                                
+                                    <Card sx = {{margin:'30px'}}>
+                                        <CardHeader title={product.ProductName}
+                                            action={
+                                                <IconButton id='iconId' aria-controls={open ? 'menus' : undefined} aria-haspopup='true' aria-expanded={open ? 'true' : undefined}>
+                                                    <MoreVertIcon  onClick={(event)=>handleClickIcon(event,index,product)}/>
+                                                </IconButton>
+                                            }
+                                        />
+                                        <CardMedia
+                                            component='img'
+                                            height='140'
+                                            image={Items}
+                                        />
+                                        <CardContent>
+                                            <Typography varient="body-2" color="text.secondary">Rs.{product.ProductPrice}</Typography>
+
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size='small'>Share</Button>
+                                            <Button size='small'>Learn More</Button>
+
+                                        </CardActions>
+                                    </Card>
+            
+                                <Dialog
+                                    open={dialogOpen}
+                                    onClose={handleClickClose}
+                                    aria-labelledby="alert-dialog-title"
+                                    aria-describedby="alert-dialog-description"
+                                >
+                                    <DialogTitle id="alert-dialog-title">
+                                        {"Delete Product"}
+                                    </DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText id="alert-dialog-description">
+                                            Do you want to delete this product item
+                                        </DialogContentText>
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button onClick={handleClickClose}>cancel</Button>
+                                        <Button onClick={handleOk} autoFocus>
+                                            Ok
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
+
+                            </div>
+                            </Grid>
+                        )
+                       
+                    })
+                }
+                </Grid>
+    
+        </>
+
+    )
+}
